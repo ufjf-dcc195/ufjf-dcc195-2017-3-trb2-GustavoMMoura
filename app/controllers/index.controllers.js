83ff100686699a823e5a59854ef54a44b32472ca
Gustavo Magalhães Moura <gustavo.magalhaes@delage.com.br>
2017-11-21 17:24:10 -0200

    Inclusão de página index e sobre.

--- app/controllers/index.controllers.js
+++ app/controllers/index.controllers.js
@@ -1,21 +1,28 @@
-module.exports.home = function (req, res, next){
-  res.send("<h1>Olá mundo!</h1>");
+module.exports.indexEJS = function (req, res, next) {
+  res.render('index', { titulo: "DCC195 - Trabalho 2", links: [{ name: "Sobre", url: "sobre.html" }] });
 }
 
-module.exports.indexEJS = function (req, res, next){
-  res.render('index', {titulo:"Olá EJS!"});
+module.exports.sobreEJS = function (req, res, next) {
+  res.render('sobre', {
+    titulo: "DCC195 - Trabalho 2 - Sobre",
+    nome: "Gustavo Magalhães Moura",
+    matricula: "201235015",
+    email: "gmmoura@ice.ufjf.br",
+    curso: "Ciência da Computação"
+  });
 }
 
-module.exports.listaEJS = function (req, res, next){
-  res.render('lista', {titulo:"Lista", a:3, b:13});
+module.exports.listaEJS = function (req, res, next) {
+  res.render('lista', { titulo: "Lista", a: 3, b: 13 });
 }
 
-module.exports.cookiesEJS = function (req, res, next){
+module.exports.cookiesEJS = function (req, res, next) {
   req.session.i = req.session.i || 1;
   req.session.ultimoAcesso = new Date();
 
-  res.render('estado', {titulo:"Estado",
-    i:req.session.i++,
-    data:req.session.ultimoAcesso
+  res.render('estado', {
+    titulo: "Estado",
+    i: req.session.i++,
+    data: req.session.ultimoAcesso
   });
 }
--- app/routes/index.routes.js
+++ app/routes/index.routes.js
@@ -1,7 +1,7 @@
 module.exports = function(app){
   var index = require("../controllers/index.controllers");
-  app.use("/ejs.html", index.indexEJS);
+  app.use("/sobre.html", index.sobreEJS);
   app.use("/lista.html", index.listaEJS);
   app.use("/biscoito.html", index.cookiesEJS);
-  app.use("/", index.home);
+  app.use("/", index.indexEJS);
 };
